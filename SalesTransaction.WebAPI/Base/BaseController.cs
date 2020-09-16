5962af6a2da0055ee64756063bb514bd584d4fbe
hsinoj1996 <hsinojjonish@gmail.com>
2020-09-16 01:37:56 -0800

    commit done

--- SalesTransaction.WebAPI/Base/BaseController.cs
+++ SalesTransaction.WebAPI/Base/BaseController.cs
@@ -1,17 +1,17 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Cors;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
 
 
-namespace SalesTransaction.WebAPI.Areas
+namespace SalesTransaction.WebAPI.Areas { 
   [Produces("application/json")]
-  [EnableCors("AllowOrigin"), Route("api/[controller]/[action]/{id?}")]
+[EnableCors("AllowOrigin"), Route("api/[controller]/[action]/{id?}")]
 public class BaseController
-    {
+{
 
-    }
 }
+}
+
